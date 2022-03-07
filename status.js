bef1dc638b82cd97214ee3d82d0b8af3337a01e8
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-03-07 13:29:31 -0300

    new functions in status.js

--- status.js
+++ status.js
@@ -4,22 +4,37 @@ const log = require('./logger')
 setInterval(() => {
     const { freemem, totalmem } = os
 
-    const total = parseInt(totalmem() / 1024 / 1024)
-    const mem = parseInt(freemem() / 1024 / 1024)
-    const usage_mem = total-mem
-    const percents = parseInt((usage_mem / total) * 100)
-    
-    const stats = {
-        time: `${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}`,
+    const total = BytesToMegaBytes(totalmem())
+    const mem = BytesToMegaBytes(freemem())
+    const usage_mem = total - mem
+    const percents = ToPercentValue(usage_mem / total)
+
+    const status = {
+        time: `${GetISOFormattedDate()}`,
         free: `${mem} MB`,
         total: `${total} MB`,
         usage: `${percents}%`,
     }
-    
-    console.clear()
-    console.log("===== PC MEM STATUS =====")
-    console.table(stats)
 
-    log(`${JSON.stringify(stats)}\n`)
+    DisplayMemotyStatus(status)
+    log(`${JSON.stringify(status)}\n`)
 
 }, 1000)
+
+function BytesToMegaBytes(memory) {
+    return parseInt(memory / 1024 / 1024)
+}
+
+function ToPercentValue(value) {
+    return parseInt(value * 100)
+}
+
+function DisplayMemotyStatus(memory_status){
+    console.clear()
+    console.log("===== PC MEM STATUS =====")
+    console.table(memory_status)
+}
+
+function GetISOFormattedDate(){
+    return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+}
